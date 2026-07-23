135fcb05e135d7a07f84edeeca298fe6483caf00
Anderson <andersonwang62@gmail.com>
2026-07-23 10:22:44 +0800

    fix syntax error and update gitignore

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ class Arena {
 	std::size_t offset = 0; // added to arena pointer to get pointer to remaining arena
 
 	public:
-		// Prevent implicit constructor calls such as Arena arena = 1024
+		// prevent implicit constructor calls such as Arena arena = 1024
 		explicit Arena(std::size_t size_) : size(size_) {
 			buffer = new std::byte[size_]; // array of bytes on heap
 		}
@@ -24,9 +24,9 @@ class Arena {
 		// compilers auto-generate copy constructors
 		// avoid them to prevent double-free of the same arena pointer when destructors run
 		Arena(const Arena&) = delete; // construct brand new copy
-		Arena& operator=(const Arena&) // assign copy to an alias
+		Arena& operator=(const Arena&) = delete; // assign copy to an alias
 		// prevent creating copies of Arena object as a result
-}
+};
 
 int main() {
 
