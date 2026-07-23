7c5e5b8e23cea33b6e0f1854c71fc346412660ee
Anderson <andersonwang62@gmail.com>
2026-07-23 10:50:38 +0800

    add allocate and reset func for Arena

--- main.cpp
+++ main.cpp
@@ -26,6 +26,29 @@ class Arena {
 		Arena(const Arena&) = delete; // construct brand new copy
 		Arena& operator=(const Arena&) = delete; // assign copy to an alias
 		// prevent creating copies of Arena object as a result
+
+		void* allocate(std::size_t bytes, std::size_t alignment) {
+			void* buffer_rem = buffer + offset;
+			std::size_t size_rem = size - offset; // to undergo alignment
+
+			// adjust pointer to remaining arena, and remaining capacity
+			// require bytes to allocate
+			// to judge whether enough remaining capacity for it after alignment
+			if (std::align(alignment, bytes, buffer_rem, size_rem)) {
+				// new offset calculated as total capacity -
+				// remaining capacity after alignment before allocation +
+				// bytes to allocate
+				// std::align to calculate 
+				offset = size - size_rem + bytes;
+				return buffer_rem;
+			}
+
+			throw std::bad_alloc();
+		}
+
+		void reset() noexcept {
+			offset = 0; // allow allocation from beginning of arena
+		}
 };
 
 int main() {
